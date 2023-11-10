a6274e44a5332c983c8d93802510f0584bf4d3e2
ArmandoMatheus <107515189+ArmandoMatheus@users.noreply.github.com>
2023-11-10 11:39:10 -0300

    DifControl inicial

---
+++ TCC-Jogo/Assets/Script/DifControlScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifControlScript : MonoBehaviour
+{
+    int[] difficulty;
+
+    int timerVal;
+    int pneulifeVal, enemylifeVal;
+    int pneyVelVal, enemyVelVal;
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
